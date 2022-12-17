2163112b2ab0f2072d0938eb98f1228ec6591821
QuackerOmega <98679693+QuackerOmega@users.noreply.github.com>
2022-12-17 22:41:00 +0800

    Update copesp.lua

--- copesp.lua
+++ copesp.lua
@@ -5,14 +5,14 @@ local clr = Color3.new(1, 0, 0)
 local clr2 = Color3.new(1,0,1)
 
 
-function DrawDrop(drop)
-    local DropText = Drawing.new("Text")
-    DropText.Visible = false
-    DropText.Center = true
-    DropText.Outline = false
-    DropText.Font = 1
-    DropText.Size = 20
-    DropText.Color = clr
+function Drawcop(cop)
+    local copText = Drawing.new("Text")
+    copText.Visible = false
+    copText.Center = true
+    copText.Outline = false
+    copText.Font = 1
+    copText.Size = 20
+    copText.Color = clr
 
     local box = Drawing.new("Square")
     box.Visible = true
@@ -24,24 +24,24 @@ function DrawDrop(drop)
 
     local function UPDATER()
         game:GetService("RunService").RenderStepped:Connect(function()
-            if drop and workspace:FindFirstChild(drop.Name) and drop:FindFirstChild("DriveSeat") then
-                if drop.DriveSeat then
-                DropText.Text = drop.Name .. " | Dist : ".. tostring(math.floor(game.Players.LocalPlayer:DistanceFromCharacter(drop.DriveSeat.CFrame.Position)+0.5))
-                local dropvector, onscreen = camera:WorldToViewportPoint(drop.DriveSeat.Position)
+            if cop and workspace:FindFirstChild(cop.Name) and cop:FindFirstChild("DriveSeat") and cop:FindFirstChild("Superior") then
+                if cop.DriveSeat then
+                copText.Text = cop.Name .. " | Dist : ".. tostring(math.floor(game.Players.LocalPlayer:DistanceFromCharacter(cop.DriveSeat.CFrame.Position)+0.5))
+                local copvector, onscreen = camera:WorldToViewportPoint(cop.DriveSeat.Position)
 
                 if onscreen then
-                    DropText.Position = Vector2.new(dropvector.X, dropvector.Y)
-                    box.Position = Vector2.new(dropvector.X-10, dropvector.Y+ -10)
+                    copText.Position = Vector2.new(copvector.X, copvector.Y)
+                    box.Position = Vector2.new(copvector.X-10, copvector.Y+ -10)
                     box.Visible = true
-                    DropText.Visible = true
+                    copText.Visible = true
                 else
-                    DropText.Visible = false
+                    copText.Visible = false
                     box.Visible = false
 
                 end
                 end
             else
-                DropText.Visible = false
+                copText.Visible = false
                 box.Visible = false
 
             end
@@ -50,14 +50,14 @@ function DrawDrop(drop)
     coroutine.wrap(UPDATER)()
 end
 
-function ddc(drop)
-    local DropText = Drawing.new("Text")
-    DropText.Visible = false
-    DropText.Center = true
-    DropText.Outline = false
-    DropText.Font = 1
-    DropText.Size = 20
-    DropText.Color = clr2
+function ddc(cop)
+    local copText = Drawing.new("Text")
+    copText.Visible = false
+    copText.Center = true
+    copText.Outline = false
+    copText.Font = 1
+    copText.Size = 20
+    copText.Color = clr2
 
     local box = Drawing.new("Square")
     box.Visible = true
@@ -70,24 +70,24 @@ function ddc(drop)
     local function upd()
         game:GetService("RunService").RenderStepped:Connect(function()
             
-            if drop and game.Players:FindFirstChild(drop.Name) and drop.Character:FindFirstChild("Head") then
-                if drop.Character.Head then
-                DropText.Text = drop.Name .. " | Dist : ".. tostring(math.floor(game.Players.LocalPlayer:DistanceFromCharacter(drop.Character.Head.CFrame.Position)+0.5))
-                local dropvector, onscreen = camera:WorldToViewportPoint(drop.Character.Head.Position)
+            if cop and game.Players:FindFirstChild(cop.Name) and cop.Character:FindFirstChild("Head") then
+                if cop.Character.Head and cop.Team.Name == "Polis DiRaja Malaysia" or cop.Team.Name == "Polis Bantuan MyTransit" then
+                copText.Text = cop.Name .. " | Dist : ".. tostring(math.floor(game.Players.LocalPlayer:DistanceFromCharacter(cop.Character.Head.CFrame.Position)+0.5))
+                local copvector, onscreen = camera:WorldToViewportPoint(cop.Character.Head.Position)
 
                 if onscreen then
-                    DropText.Position = Vector2.new(dropvector.X, dropvector.Y)
-                    box.Position = Vector2.new(dropvector.X-10, dropvector.Y+ -10)
+                    copText.Position = Vector2.new(copvector.X, copvector.Y)
+                    box.Position = Vector2.new(copvector.X-10, copvector.Y+ -10)
                     box.Visible = true
-                    DropText.Visible = true
+                    copText.Visible = true
                 else
-                    DropText.Visible = false
+                    copText.Visible = false
                     box.Visible = false
 
                 end
                 end
             else
-                DropText.Visible = false
+                copText.Visible = false
                 box.Visible = false
 
             end
@@ -100,37 +100,37 @@ end
 -- cop players
 local tagged = {}
 
-    for i,v in next, game.Players:GetPlayers() do
-        if v.Team.Name == "Polis DiRaja Malaysia" or v.Team.Name == "Polis Bantuan MyTransit" then
-            ddc(v)
-        end 
-    end
+for i,v in next, game.Players:GetPlayers() do
+    if v.Team.Name == "Polis DiRaja Malaysia" or v.Team.Name == "Polis Bantuan MyTransit" then
+        ddc(v)
+        table.insert(tagged,v.Name)
+    end 
+end
 
-    game:GetService("RunService").RenderStepped:Connect(function()
-        for i,drop in next, game.Players:GetPlayers() do
-            if drop ~= game.Players.LocalPlayer then
-                if drop.Team.Name == "Polis DiRaja Malaysia" or drop.Team.Name == "Polis Bantuan MyTransit" then
-                    for i,v in next, tagged do
-                        if drop.Name ~= v then    
-                        ddc(drop)
-                        table.insert(tagged,drop.Name)
-                        end
-                    end
+game.Players.PlayerAdded:Connect(function(v)
+    v.CharacterAdded:Connect(function()
+        if v.Team.Name == "Polis DiRaja Malaysia" or v.Team.Name == "Polis Bantuan MyTransit" then
+            for g,b in next, tagged do
+                if v.Name ~= b then
+                    ddc(v)
+                    table.insert(tagged,v.Name)
                 end
             end
         end
     end)
+end)
 
 
     -- cop cars
     for i,v in next, workspace:GetChildren() do
         if v:FindFirstChild("A-Chassis Tune") and v:FindFirstChild("Superior") then
-            DrawDrop(v)
+            Drawcop(v)
         end
     end
 
-    game:GetService("Workspace").ChildAdded:Connect(function(drop)
-        if drop:FindFirstChild("A-Chassis Tune") and drop:FindFirstChild("Superior") then
-        DrawDrop(drop)
+    game:GetService("Workspace").ChildAdded:Connect(function(cop)
+        wait(1)
+        if cop and workspace:FindFirstChild(cop.Name) and cop:FindFirstChild("DriveSeat") and cop:FindFirstChild("Superior") then
+            Drawcop(cop)
         end
     end)
